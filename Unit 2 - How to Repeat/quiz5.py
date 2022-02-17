3d0bd7608b5614d00fe9c9a9a8240b2abd9ed13e
Phillip <98851253+phillipai@users.noreply.github.com>
2022-02-17 17:36:01 -0500

    Create quiz5.py

---
+++ Unit 2 - How to Repeat/quiz5.py
@@ -0,0 +1,21 @@
+# Define a procedure, bigger, that takes in
+# two numbers as inputs, and returns the
+# greater of the two inputs.
+
+def bigger(number1, number2):
+    if number1 > number2:
+        return number1
+    return number2
+
+
+
+
+
+#print bigger(2,7)
+#>>> 7
+
+print bigger(3,2)
+#>>> 3
+
+#print bigger(3,3)
+#>>> 3
