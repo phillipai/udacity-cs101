0e99d2eb1c8c9a9201dd44e1188f9874407c5891
Phillip <98851253+phillipai@users.noreply.github.com>
2022-02-17 17:29:13 -0500

    Create quiz8.py

---
+++ Unit 1 - How to Get Started/quiz8.py
@@ -0,0 +1,19 @@
+# Write Python code that assigns to the 
+# variable url a string that is the value 
+# of the first URL that appears in a link 
+# tag in the string page.
+# Your code should print http://udacity.com
+# Make sure that if page were changed to
+
+# page = '<a href="http://udacity.com">Hello world</a>'
+
+# that your code still assigns the same value to the variable 'url', 
+# and therefore still prints the same thing.
+
+# page = contents of a web page
+page =('<div id="top_bin"><div id="top_content" class="width960">'
+'<div class="udacity float-left"><a href="http://udacity.com">')
+start_link = page.find('<a href=')
+start_quote = page.find('"', start_link)
+end_quote = page.find('"',start_quote + 1)
+url = page[start_quote + 1: end_quote]
