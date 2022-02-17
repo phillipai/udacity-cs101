14591c18c7c7c08581f24b168f7c574811e625c7
Phillip <98851253+phillipai@users.noreply.github.com>
2022-02-17 18:12:28 -0500

    Create quiz2.py

---
+++ Unit 7/Cumulative Practice Problems/quiz2.py
@@ -0,0 +1,30 @@
+# Question 2. Triangular Numbers
+
+# The triangular numbers are the numbers 1, 3, 6, 10, 15, 21, ...
+# They are calculated as follows.
+
+# 1
+# 1 + 2 = 3
+# 1 + 2 + 3 = 6
+# 1 + 2 + 3 + 4 = 10
+# 1 + 2 + 3 + 4 + 5 = 15
+
+# Write a procedure, triangular, that takes as its input a positive 
+# integer n and returns the nth triangular number.
+
+def triangular(n):
+    total = 0
+    for e in range(0, n + 1):
+        total += e
+    return total
+
+
+
+print triangular(1)
+#>>>1
+
+print triangular(3)
+#>>> 6
+
+print triangular(10)
+#>>> 55
