6e836e2cb9e0611074d19ed3d95da2cfa70e231a
Phillip <98851253+phillipai@users.noreply.github.com>
2022-02-17 17:36:10 -0500

    Create quiz6.py

---
+++ Unit 2 - How to Repeat/quiz6.py
@@ -0,0 +1,27 @@
+# Define a procedure, is_friend, that
+# takes a string as its input, and
+# returns a Boolean indicating if
+# the input string is the name of
+# a friend. Assume I am friends with
+# everyone whose name starts with D
+# and no one else. You do not need to
+# check for the lower case 'd'
+
+def is_friend(name):
+    if name[0] != 'D':
+        return False
+    else:
+        return True
+    
+        
+
+
+
+
+
+
+#print is_friend('Diane')
+#>>> True
+
+print is_friend('Fred')
+#>>> False
