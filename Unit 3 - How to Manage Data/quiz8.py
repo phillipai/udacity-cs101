ff1a2e35c73a102baf4652b82f32585f9ace2e45
Phillip <98851253+phillipai@users.noreply.github.com>
2022-02-17 17:46:50 -0500

    Create quiz8.py

---
+++ Unit 3 - How to Manage Data/quiz8.py
@@ -0,0 +1,25 @@
+# Define a procedure, find_element,
+# that takes as its inputs a list
+# and a value of any type, and
+# returns the index of the first
+# element in the input list that
+# matches the value.
+
+# If there is no matching element,
+# return -1.
+
+def find_element(list, value):
+    index = 0
+    for e in list:
+        if e == value:
+            return index
+        index += 1
+    else:
+        return -1
+    
+
+print find_element([1,2,3],3)
+#>>> 2
+
+#print find_element(['alpha','beta'],'gamma')
+#>>> -1
