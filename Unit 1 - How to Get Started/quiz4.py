262f0299434a1e8a9437321e8df1c29078d8631f
Phillip <98851253+phillipai@users.noreply.github.com>
2022-02-17 17:27:47 -0500

    Create quiz4.py

---
+++ Unit 1 - How to Get Started/quiz4.py
@@ -0,0 +1,8 @@
+# Write python code that defines the variable 
+# age to be your age in years, and then prints 
+# out the number of days you have been alive.
+
+age = 24
+daysinyear = 365
+
+print (age * daysinyear)
