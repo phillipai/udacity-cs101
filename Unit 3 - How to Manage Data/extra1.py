9109d4b9cb35ccaaec901e34cbaca5d5075c607c
Phillip <98851253+phillipai@users.noreply.github.com>
2022-02-17 17:51:26 -0500

    Create extra1.py

---
+++ Unit 3 - How to Manage Data/extra1.py
@@ -0,0 +1,14 @@
+# Investigating adding and appending to lists
+# If you run the following four lines of codes, what are list1 and list2?
+
+list1 = [1,2,3,4]
+list2 = [1,2,3,4]
+
+list1 = list1 + [5, 6]
+list2.append([5, 6])
+
+# to check, you can print them out using the print statements below.
+
+print "showing list1 and list2:"
+print list1
+print list2
