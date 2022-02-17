2acdec9b5426837001601adb46bd465e12c0feb6
Phillip <98851253+phillipai@users.noreply.github.com>
2022-02-17 17:36:48 -0500

    Create quiz9.py

---
+++ Unit 2 - How to Repeat/quiz9.py
@@ -0,0 +1,21 @@
+# Define a procedure, print_numbers, that takes
+# as input a positive whole number, and prints 
+# out all the whole numbers from 1 to the input
+# number.
+
+# Make sure your procedure prints "upwards", so
+# from 1 up to the input number.
+
+                  
+def print_numbers(whole_number):
+    whole_number2 = 0
+    while whole_number > whole_number2:
+        whole_number2 = whole_number2 + 1
+        print whole_number2
+
+
+
+print_numbers(3)
+#>>> 1
+#>>> 2
+#>>> 3
